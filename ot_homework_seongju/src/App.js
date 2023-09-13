d26bfd1013a0d16a1317d4aa046abac29cc3804c
seongju1015 <gmrfkels1015@gmail.com>
2023-09-13 23:24:26 +0900

    upload middle course

--- ot_homework_seongju/src/App.js
+++ ot_homework_seongju/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+
+function Introduce(){
+  return(
+    <div>
+      <h1>💡Introduce me💡</h1>
+      <p>안녕하세요 저는 20학번 이성주 입니다.</p>
+      <img style = {{display: "block"}} src="https://user-images.githubusercontent.com/122343583/267662354-d8cec815-8e54-4cec-9c53-bf814bc8e0b2.jpeg" alt="" />
+      <p>👣ISTP</p>
+      <p></p>
+      <p>이번학기 목표 : 계획형 인간으로 변하기 </p>
+      
+
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="자기소개">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">
+
+      🏄<Introduce />🏄
     </div>
+    
   );
 }
 
