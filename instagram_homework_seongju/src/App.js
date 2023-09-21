5dff8248b4fc45364a959ed053e560520378300c
seongju1015 <gmrfkels1015@gmail.com>
2023-09-22 01:54:50 +0900

    upload 1st draft

--- instagram_homework_seongju/src/App.js
+++ instagram_homework_seongju/src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+/*import를 하지 않고 public폴더에 사진 넣어두고 꺼내 쓰는 방식 사용. */
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "whole_body">
+      <div className = "body">
+        <div className = "insta_logo">
+          <img src = "/Navigations.png" />
+          <img src = "NewPosts.png" alt ="더하기로고" className="plus_logo"/>
+          <img src = "miniProfile.png" alt ="작은프로필" className="mini_profile"/>
+        </div>
+
+        <div className = "profile">
+          <img src = "miniProfile.png" alt="메인프로필" className="main_profile"/>
+          <img src = "Desc.png" alt="프로필소개내용" className="profile_text"/>
+          <div className='post'>3</div>
+          <div className='follower'>777</div>
+          <div className='follow'>1</div>
+        </div>
+
+        <div className='post'>
+          <img src ="Toogle.png" alt="헤더메뉴" className='header_menu'/>
+          <img src="HipCat.png" alt="파드사진1" className='peed1'/>
+          <img src="234.png" alt="파드사진2" className='peed2'/>
+          <img src="Original.png" alt="파드로고" className='pard_logo'/>
+          <img src="HipCat2.png" alt="피드사진3" className='peed3'/>
+        </div>
+      </div>
     </div>
+
   );
 }
 
---
+++ instagram_homework_seongju/src/Profile.js
@@ -0,0 +1,9 @@
+function Profile(){
+    <div className = "logo">
+        <div className = "logo">
+            <p>instagram Icons/NewPosts</p>
+        </div>
+
+    </div>
+}
+export default Profile;
