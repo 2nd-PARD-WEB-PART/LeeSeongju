df58a29ccf26a9832ce8fd9dece59124a055058a
seongju1015 <gmrfkels1015@gmail.com>
2023-11-03 03:00:54 +0900

    App.js파일 주석 작성

--- instagram_homework_seongju/src/App.js
+++ instagram_homework_seongju/src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
 import EditProfile from './EditProfile'; 
 /*import를 하지 않고 public폴더에 사진 넣어두고 꺼내 쓰는 방식 사용. */
 
-export const MyContext = createContext();
+export const MyContext = createContext();/** Context이용 위해서 선언 */
 
 function App() {
   const [MyData, setUser] = useState({
@@ -18,16 +18,19 @@ function App() {
     gender:'Male',
     id:'sj_mon_',
   });
+  /** 저장할 기본적인 data값들. Context를 이용해서 바꿔줌 값들임. */
 
   const [likeCount, setLikeCount] = useState(77);
   const [isLiked, setIsLiked] = useState(false);
   const [comments, setComments] = useState([]); /**댓글 목록 상태 */
+  /** useState값을 Context사용해서 변경하기 위해서 App.js에 선언해놓음. */
 
   return (
     <MyContext.Provider value={{MyData, setUser,
     likeCount,setLikeCount,
     isLiked, setIsLiked,
     comments, setComments}}>
+      {/** Context사용 위해서 Provider 사용 후 value값으로 변경 되어야하는 값들 넣어줌. */}
       <Router>
         <Routes>
           <Route path="/edit-profile" element={<EditProfile />} />
