30f56463e49c92943cbab3a21a9d2fec6d1cb166
seongju1015 <gmrfkels1015@gmail.com>
2023-11-28 01:28:57 +0900

    에니메이션 연습..

--- training_router/src/App.js
+++ training_router/src/App.js
@@ -1,21 +1,29 @@
-import { BrowserRouter as Router, Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Page from './Page.js';
 import Home from './Home.js';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path='/page' element={<Page/>}> go</Route>
-          <Route path='/' element={<Home/>}>home</Route>
-        </Routes>
-        
-      </Router>
-      
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={(
+            <TransitionGroup>
+              <CSSTransition classNames="fade" timeout={500}>
+                <Routes>
+                  <Route path='/' element={<Home />} />
+                  <Route path='/page' element={<Page />} />
+                </Routes>
+              </CSSTransition>
+            </TransitionGroup>
+          )}
+        />
+      </Routes>
+    </Router>
   );
 }
 
-export default App
+export default App;
--- training_router/src/Home.js
+++ training_router/src/Home.js
@@ -1,14 +1,20 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import {motion} from "framer-motion";
 
 function Home(){
     return(
-        <div>
+        <motion.div
+      className="loginPage"
+      intial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
             <p>This is main page</p>
             <Link to='/page'>
                 <button>go second page</button>
             </Link>
-        </div>
+        </motion.div>
     );
 }
 
